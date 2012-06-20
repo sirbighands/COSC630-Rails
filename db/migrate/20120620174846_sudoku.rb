2d5d97f22e7f039a0acafc011224f2ae2251830f
sirbighands <sirbighands@gmail.com>
2012-06-20 17:59:14 -0400

    update

--- db/migrate/20120620174846_sudoku.rb
+++ db/migrate/20120620174846_sudoku.rb
@@ -1,10 +1,7 @@
 class Sudoku < ActiveRecord::Migration
-	def change
-    create_table :sudokus do |t|
-      t.string :puzzle
-      t.string :difficulty
- 
-      t.timestamps
-    end
-	end
+def self.up
+    add_column  t.string :difficulty
+  end
+
+
 end
