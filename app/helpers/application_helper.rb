4cbffdbee1b8a7b065fde61fb01228ecd971cf8d
sirbighands <sirbighands@gmail.com>
2012-06-20 13:08:21 -0400

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the title for the application
+  def title
+    base_title = "Sudoku Generator and player"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title}"
+    end
+  end
 end
